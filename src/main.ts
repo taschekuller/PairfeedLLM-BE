359379530fa77ddeaa77ccf698d2d70996fb77b7
taschekuller <idogukanince@gmail.com>
2025-02-09 21:20:17 +0300

    main :: added cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  }));
+
   await app.listen(5001);
 }
 bootstrap();
